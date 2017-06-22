0bc3b2c4f63772f65bf8aa3666fbeec66bd07581
Matias Rasmussen <rasmussen.matias@gmail.com>
2017-06-22 14:50:19 +0200

    Rewirte middleware to match django 1.10 style.

--- django_banana/middleware.py
+++ django_banana/middleware.py
@@ -7,8 +7,16 @@ class ExperimentsMiddleware(object):
     Binds the convenient :class:`ExperimentManager` to the request.
     """
 
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
         assert hasattr(request, 'session'), \
             'Django Banana requires session middleware to be installed'
 
         request.experiments = ExperimentManager(request.session)
+        # Get response from later middleware.
+        response = self.get_response(request)
+
+        return response
